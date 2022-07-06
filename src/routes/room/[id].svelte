78a635d16c905d58e4fbcb99e04d8c67f2d8a90a
alessrdrgz <alessrodrgz@gmail.com>
2022-07-07 00:31:18 +0200

    :construction:feat: Dynamic chat route

---
+++ src/routes/room/[id].svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Conversation from '../../components/Conversation.svelte';
+	import ConversationInput from '../../components/ConversationInput.svelte';
+	import { activeConversation } from '../../store/conversationStore';
+	console.log({ $activeConversation });
+</script>
+
+{#if $activeConversation}
+	<div class="max-w-6xl mx-auto py-2 relative h-full">
+		<h2 class="text-3xl">{$activeConversation.uniqueName}</h2>
+		<Conversation />
+		<ConversationInput />
+	</div>
+{/if}

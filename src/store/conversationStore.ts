5be6fa757a432323cb22d0e03ca8cbd4c82b4e15
alessrdrgz <alessrodrgz@gmail.com>
2022-07-07 14:40:15 +0200

    :sparkles:feat: active conversations users store

--- src/store/conversationStore.ts
+++ src/store/conversationStore.ts
@@ -1,4 +1,6 @@
 import type { Conversation } from '@twilio/conversations';
+import type { UserProfile } from '../services/user';
 import { writable } from 'svelte/store';
 
 export const activeConversation = writable<Conversation | null>(null);
+export const activeConversationUsers = writable<Array<UserProfile>>([]);

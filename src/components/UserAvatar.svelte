ddd6738d97b59741995d265dd7509765cb5b1544
alessrdrgz <alessrodrgz@gmail.com>
2022-07-07 14:41:13 +0200

    :sparkles:feat: user avatar component

---
+++ src/components/UserAvatar.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let avatar: string;
+</script>
+
+<div
+	id="user-avatar"
+	class="flex-shrink-0 flex-grow-0 basis-auto w-16 h-16 -mt-3 bg-contain rounded-lg shadow-md"
+	style="background-image: url('{avatar}')"
+/>

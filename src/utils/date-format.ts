06a3ee3966247c0130def460d813abaf8a43a1d0
alessrdrgz <alessrodrgz@gmail.com>
2022-07-07 14:37:55 +0200

    :sparkles:feat: date format function

---
+++ src/utils/date-format.ts
@@ -0,0 +1,17 @@
+export function formatDate({ date }: { date: Date }) {
+	const { locale, timeZone } = Intl.DateTimeFormat().resolvedOptions();
+	const options: Intl.DateTimeFormatOptions = {
+		day: '2-digit',
+		month: '2-digit',
+		hour12: false,
+		year: 'numeric',
+		hour: '2-digit',
+		minute: '2-digit',
+		timeZone
+	};
+
+	const formatter = new Intl.DateTimeFormat(locale, options);
+	const formattedDate = formatter.format(date);
+	const today = new Date().toDateString() == date.toDateString();
+	return today ? formattedDate.split(',')[1] : formattedDate;
+}

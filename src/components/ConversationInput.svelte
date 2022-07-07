9295211e3290a46f948f884270e68b3a4ddd7c87
alessrdrgz <alessrodrgz@gmail.com>
2022-07-07 14:44:41 +0200

    :sparkles:feat: added emoji picker

--- src/components/ConversationInput.svelte
+++ src/components/ConversationInput.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
 	import { activeConversation } from '../store/conversationStore';
+	import { EmojiButton } from '@joeattardi/emoji-button';
 
 	let message = '';
 
+	let emojiButton: HTMLElement;
+	const emojiPicker = new EmojiButton();
+
+	emojiPicker.on('emoji', ({ emoji }) => {
+		message += emoji;
+	});
+
 	const handleKeyDown = (e: KeyboardEvent) => {
-		if (e.key === 'Enter') {
+		if (e.key === 'Enter' && message !== '') {
 			$activeConversation?.sendMessage(message);
 			message = '';
 		}
@@ -13,11 +21,17 @@
 
 <svelte:window on:keydown={handleKeyDown} />
 
-<div class="mt-5">
+<div class="mt-5 flex px-4" id="input-container">
 	<input
-		class="p-2 border-gray-700 w-full border-2"
+		class="p-2 flex-1 border-gray-700 border-2 rounded-xl"
 		type="text"
 		bind:value={message}
 		placeholder="Escribe tu mensaje aquí"
 	/>
+	<button
+		bind:this={emojiButton}
+		on:click={() => emojiPicker.togglePicker(emojiButton)}
+		class="ml-2"
+		id="emoji-picker">😄</button
+	>
 </div>

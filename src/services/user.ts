01dd37fe240107a5e1f274730440b23cc8396603
alessrdrgz <alessrodrgz@gmail.com>
2022-07-07 14:39:14 +0200

    :sparkles:feat: create user profile on supabase db on user sign up

--- src/services/user.ts
+++ src/services/user.ts
@@ -2,7 +2,7 @@ import { supabase } from './supabase';
 import { user, type User } from '../store/userStore';
 import type { Provider, Session } from '@supabase/supabase-js';
 
-const parseUserData = (session: Session): User | null => {
+const parseUserData = async (session: Session): Promise<User | null> => {
 	if (session && session.user) {
 		const { user } = session;
 		const { user_metadata, id, email } = user;
@@ -14,6 +14,7 @@ const parseUserData = (session: Session): User | null => {
 			avatar_url: avatar_url ?? `https://i.pravatar.cc/1000?u=${session.user.id}`,
 			token: session.access_token
 		};
+		await createUserProfile({ id, username, avatar_url });
 		return userData;
 	} else return null;
 };
@@ -21,7 +22,7 @@ const parseUserData = (session: Session): User | null => {
 export async function signIn({ provider }: { provider: string }) {
 	const { session, error } = await supabase.auth.signIn({ provider: provider as Provider });
 	if (!error && session && session.user) {
-		user.set(parseUserData(session));
+		user.set(await parseUserData(session));
 	}
 	return { session, error };
 }
@@ -30,8 +31,8 @@ export async function logout() {
 	await supabase.auth.signOut();
 }
 
-supabase.auth.onAuthStateChange((_evt, session) => {
-	if (session) user.set(parseUserData(session));
+supabase.auth.onAuthStateChange(async (_evt, session) => {
+	if (session) user.set(await parseUserData(session));
 	else user.set(null);
 });
 
@@ -46,4 +47,33 @@ export async function getAccessToken({ token }: { token: string }) {
 
 	const { accessToken } = await res.json();
 	return accessToken;
+}
+
+export type UserProfile = {
+	id: string;
+	username: string;
+	avatar_url: string;
+	accent_color?: string;
+};
+
+async function createUserProfile({
+	id,
+	username,
+	avatar_url
+}: {
+	id: string;
+	username: string;
+	avatar_url: string;
+}) {
+	const { data, error } = await supabase
+		.from<UserProfile>('profiles')
+		.select()
+		.filter('id', 'eq', id);
+	if (!error) {
+		if (data.length > 0) {
+			await supabase.from<UserProfile>('profiles').update({ username }).match({ id });
+		} else {
+			await supabase.from<UserProfile>('profiles').insert({ id, username, avatar_url });
+		}
+	} else throw error;
 }

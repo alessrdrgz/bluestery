00c35bc64cf5243a025e446b5b58a4c192c4560b
alessrdrgz <alessrodrgz@gmail.com>
2022-07-11 19:21:29 +0200

    :lipstick:style: styled the modal for adding a user

--- src/components/AddUserModal.svelte
+++ src/components/AddUserModal.svelte
@@ -1,25 +1,45 @@
 <script lang="ts">
-	import { Conversation } from '@twilio/conversations';
-
+	import { getAllUsers } from '../services/user';
 	import { addUserToConversation } from '../services/chat';
-
 	import { activeConversation } from '../store/conversationStore';
-	let username = '';
+	import PrimaryButton from './buttons/Primary.svelte';
+	import AutoComplete from './AutoCompleteInput.svelte';
+
+	let selectedUser: string;
+	let responseMessage: string;
+	let responseError: boolean = false;
+	const userPromise = getAllUsers();
+
+	const handleAddUser = async () => {
+		if ($activeConversation && selectedUser) {
+			console.log(selectedUser);
+			const { message, error } = await addUserToConversation({
+				conversation: $activeConversation,
+				username: selectedUser
+			});
+
+			responseMessage = message;
+			responseError = error ?? false;
+			console.log({ message, error });
+		}
+	};
 </script>
 
-<div class="h-32 w-fit m-auto">
-	<input placeholder="Nombre de usuario" name="username" bind:value={username} />
-	<ul class="grid grid-cols-2">
-		<li>
-			<button>Cancelar</button>
-		</li>
-		<li>
-			{#if $activeConversation !== null}
-				<button
-					on:click={() => addUserToConversation({ conversation: $activeConversation, username })}
-					>Añadir</button
-				>
-			{/if}
-		</li>
-	</ul>
-</div>
+{#await userPromise then users}
+	<div class="h-fit w-fit m-auto flex items-center flex-col mb-16">
+		<AutoComplete
+			data={users}
+			placeholder="Nombre de usuario"
+			objectKey="username"
+			bind:inputValue={selectedUser}
+		/>
+	</div>
+	<div class="text-center">
+		<p class={`${responseError && 'text-red'} text-center mb-3`}>
+			{responseMessage || ' '}
+		</p>
+		{#if $activeConversation !== null}
+			<PrimaryButton on:click={handleAddUser} disabled={selectedUser}>Añadir</PrimaryButton>
+		{/if}
+	</div>
+{/await}

fd45b8a5cf50836e02943b1faf16a0e56a0a6710
alessrdrgz <alessrodrgz@gmail.com>
2022-07-14 01:26:23 +0200

    :lipstick:style: styled width, input and buttons

--- src/components/AddUserModal.svelte
+++ src/components/AddUserModal.svelte
@@ -3,6 +3,7 @@
 	import { addUserToConversation } from '../services/chat';
 	import { activeConversation } from '../store/conversationStore';
 	import PrimaryButton from './buttons/Primary.svelte';
+	import RedButton from './buttons/Red.svelte';
 	import AutoComplete from './AutoCompleteInput.svelte';
 
 	let selectedUser: string;
@@ -18,13 +19,28 @@
 			});
 
 			responseMessage = message;
-			responseError = error ?? false;
+			responseError = error || false;
 		}
+	};
+
+	const handleCloseModal = (e: MouseEvent) => {
+		window.dispatchEvent(new KeyboardEvent('keydown', { key: 'Escape' }));
+		window.dispatchEvent(new KeyboardEvent('keyup', { key: 'Escape' }));
 	};
 </script>
 
 {#await userPromise then users}
-	<div class="h-fit w-fit m-auto flex items-center flex-col mb-16">
+	<h1 class="text-center mb-3 font-black text-lg">Añadir un usuario</h1>
+	{#if responseMessage}
+		<p
+			class={`${
+				responseError ? 'text-red-500' : 'text-gray-500'
+			} text-xs font-medium text-center mb-2`}
+		>
+			{responseMessage}
+		</p>
+	{/if}
+	<div class={`m-auto flex items-center flex-col mb-14${!responseError && 'mt-8'}`}>
 		<AutoComplete
 			data={users}
 			placeholder="Nombre de usuario"
@@ -33,12 +49,16 @@
 			bind:inputValue={selectedUser}
 		/>
 	</div>
-	<div class="text-center">
-		<p class={`${responseError && 'text-red'} text-center mb-3`}>
-			{responseMessage || ' '}
-		</p>
+	<div class="text-center pt-16">
 		{#if $activeConversation !== null}
-			<PrimaryButton on:click={handleAddUser} disabled={selectedUser}>Añadir</PrimaryButton>
+			<ul class="grid grid-cols-2">
+				<li>
+					<RedButton on:click={handleCloseModal}>Cancelar</RedButton>
+				</li>
+				<li>
+					<PrimaryButton on:click={handleAddUser} disabled={selectedUser}>Añadir</PrimaryButton>
+				</li>
+			</ul>
 		{/if}
 	</div>
 {/await}

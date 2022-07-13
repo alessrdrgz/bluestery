d29d6efc1e3059634fec9c99585203b91a0d5268
alessrdrgz <alessrodrgz@gmail.com>
2022-07-14 01:34:19 +0200

    :sparkles:feat: await for refreshing user session

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
 	import '../styles/global.css';
 	import '../styles/app.css';
+	import { supabase } from '../services/supabase';
+	import { refreshUserSession } from '../services/user';
+
+	const session = supabase.auth.session();
+	const userPromise = refreshUserSession({ session });
 </script>
 
-<main class="h-full w-full">
-	<slot />
-</main>
+{#await userPromise then user}
+	<main class="h-full w-full">
+		<slot />
+	</main>
+{/await}

001e763c09f078f6e8a7606e80c97b8b4f1fd8f7
alessrdrgz <alessrodrgz@gmail.com>
2022-07-14 15:30:39 +0200

    :lipstick:style: added disabled styles and transition for hover

--- src/components/buttons/Primary.svelte
+++ src/components/buttons/Primary.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	let buttonProps = {
-		class: [$$restProps.class]
+		class: $$restProps.class,
+		disabled: $$restProps.disabled
 	};
 </script>
 
@@ -12,6 +13,6 @@
 	on:mouseleave
 	on:focus
 	{...buttonProps}
-	class="text-white bg-blue-700 hover:bg-blue-80 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700"
+	class={`text-white bg-blue-700 hover:bg-blue-80 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 disabled:bg-gray-500 disabled:hover:bg-gray-500 transition-colors ease-in-out duration-500 ${buttonProps.class}`}
 	><slot /></button
 >
--- src/components/buttons/Red.svelte
+++ src/components/buttons/Red.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	let buttonProps = {
-		class: [$$restProps.class]
+		class: $$restProps.class
 	};
 </script>
 
@@ -12,6 +12,6 @@
 	on:mouseleave
 	on:focus
 	{...buttonProps}
-	class="focus:outline-none text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700
-  "><slot /></button
+	class={`focus:outline-none text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 disabled:hover:bg-gray-500 transition-colors ease-in-out duration-500 ${buttonProps.class}`}
+	><slot /></button
 >

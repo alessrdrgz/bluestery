10a684933d6cac14fd9820cb4d6d9a2971ead5b5
alessrdrgz <alessrodrgz@gmail.com>
2022-07-14 04:23:36 +0200

    :sparkles:feat: generate invitation url endpoint

---
+++ src/routes/api/twilio/generate-invite-url.ts
@@ -0,0 +1,41 @@
+import type { RequestHandler } from '@sveltejs/kit';
+import { supabase } from '../../../services/supabase';
+import { Client } from '@twilio/conversations';
+import { generateAccessToken } from './get-access-token';
+import * as JWT from 'jsonwebtoken';
+
+const { VITE_PUBLIC_JWT_SECRET: JWT_SECRET, VITE_PUBLIC_DOMAIN: DOMAIN } = import.meta.env;
+
+export const get: RequestHandler = async ({ request }) => {
+	const jwt = request.headers.get('jwt');
+	if (!jwt) return { status: 401, body: { message: 'No se ha encontrado el usuario' } };
+
+	const sid = request.headers.get('sid');
+	if (!sid) return { status: 422, body: { message: 'No se ha encontrado la sala' } };
+
+	const user = await supabase.auth.api.getUser(jwt);
+	if (!user) return { status: 401, body: { message: 'No se ha encontrado el usuario' } };
+
+	if (user.data?.id) {
+		const accessToken = generateAccessToken(user.data?.id);
+		const client = new Client(accessToken);
+		const conversation = await client.getConversationBySid(sid);
+		const conversationUser = await conversation.getParticipantByIdentity(user.data?.id);
+
+		if (conversation) {
+			if (conversationUser?.roleSid === 'RL18d041bac1f44010a8951b9347a0bc8f') {
+				const jsonWebToken = JWT.sign({ sid }, JWT_SECRET, { expiresIn: '15m' });
+				return {
+					status: 200,
+					body: {
+						url: `${DOMAIN}room/invite?token=${jsonWebToken}`
+					}
+				};
+			} else
+				return {
+					status: 401,
+					body: { message: 'Debes ser el admin de la conversación para generar una invitación' }
+				};
+		} else return { status: 404, body: { message: `Conversation with sid ${sid} not found` } };
+	} else return { status: 401 };
+};

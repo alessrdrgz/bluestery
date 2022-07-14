ce19f318426db98e3479cd1e09e627102fa35af5
alessrdrgz <alessrodrgz@gmail.com>
2022-07-14 04:36:39 +0200

    :sparkles:feat: conversation invite route

---
+++ src/routes/room/invite.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { user } from '../../store/userStore';
+	import { getAccessToken, acceptInvitation } from '../../services/user';
+	import { page } from '$app/stores';
+
+	async function checkInvitation() {
+		return new Promise<{ error?: boolean; message: string }>(async (resolve) => {
+			if ($user && $user?.token !== null) {
+				const sidToken = $page.url.searchParams.get('token');
+				if (sidToken) {
+					const accessToken = await getAccessToken({ token: $user.token });
+					if (accessToken) {
+						const res = await acceptInvitation({ sidToken, accessToken, user: $user });
+						resolve({ message: res.message });
+					} else resolve({ error: true, message: `Error al obtener el token de acceso` });
+				} else resolve({ error: true, message: 'Enlace de invitación inválido' });
+			} else resolve({ error: true, message: `Debe iniciar sesión primero` });
+		});
+	}
+
+	const invitationPromise = checkInvitation();
+</script>
+
+{#await invitationPromise then res}
+	<div class={`mx-auto`}>
+		<h1 class={`text-xl${res.error && 'text-red-500'}`}>{res.message}</h1>
+	</div>
+{/await}

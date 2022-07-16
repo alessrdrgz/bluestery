839f8d20c1b5abc9c80345ff7652192b4b600262
alessrdrgz <alessrodrgz@gmail.com>
2022-07-16 23:33:17 +0200

    :lipstick:style: styled homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,34 @@
 <script lang="ts">
 	import RoomSelect from '../components/RoomSelect.svelte';
-	import { logout, signIn } from '../services/user';
 	import { user } from '../store/userStore';
+	import LoginButton from '../components/buttons/Login.svelte';
 </script>
 
-<section class="h-full w-full bg-gray-800">
-	{#if !$user}
-		<button on:click={() => signIn({ provider: 'github' })}>Sign In with GitHub</button>
-		<button on:click={() => signIn({ provider: 'google' })}>Sign In with Google</button>
-	{:else}
-		<img src={$user.avatar_url} alt={$user.username} class="w-8 h-8 rounded-3xl" />
-		<h1>Welcome {$user.username}</h1>
-		<button on:click={logout}>Logout</button>
-		<RoomSelect />
-	{/if}
+<section class="h-full w-full">
+	<div class="h-[60px] bg-blue-700 w-full border-b border-gray-300" />
+	<div class="w-full">
+		<h1
+			class="text-transparent text-center text-6xl font-extrabold bg-clip-text bg-gradient-to-tr from-blue-300 via-blue-500 to-blue-700 mt-4"
+		>
+			Bluestery
+		</h1>
+
+		<div class="mx-auto w-fit text-2xl font-semibold mt-10 leading-10 text-center">
+			<p>Mantén todos</p>
+			<p>tus chats</p>
+			<p>en secreto</p>
+			<p>💬🔐</p>
+		</div>
+
+		{#if !$user}
+			<div class="mx-auto flex items-center justify-center flex-col mt-10 gap-4">
+				<LoginButton type="google" />
+				<LoginButton type="github" />
+			</div>
+		{:else}
+			<did class="mx-auto w-4/5">
+				<RoomSelect />
+			</did>
+		{/if}
+	</div>
 </section>

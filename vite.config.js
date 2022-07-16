bac0e78f5aaddbfcee74bff10a786b1e71e3fbae
alessrdrgz <alessrodrgz@gmail.com>
2022-07-17 00:43:23 +0200

    :wrench:build: added path aliases for imports

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'path';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			$lib: path.resolve('src/lib'),
+			$components: path.resolve('src/components'),
+			$utils: path.resolve('src/utils'),
+			$services: path.resolve('src/services'),
+			$store: path.resolve('src/store'),
+			$: path.resolve('src')
+		}
+	}
 };
 
 export default config;

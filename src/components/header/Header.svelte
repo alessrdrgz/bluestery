885745c0de0e605b1f130b5931941448f8cfe28d
alessrdrgz <alessrodrgz@gmail.com>
2022-07-19 19:17:35 +0200

    :sparkles:feat: header component

---
+++ src/components/header/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+	import UserAvatar from '$components/header/UserAvatar.svelte';
+	import { user } from '$store/userStore';
+	import { useCycle } from 'svelte-motion';
+	import SidebarMainMenu from '$components/header/SidebarMenu/Main.svelte';
+
+	const mainMenuToggle = useCycle(false, true);
+</script>
+
+<div class="h-[60px] bg-[#2c73d0] w-full border-b border-gray-300 flex items-center">
+	<a href="/"><h1 class="text-white text-2xl ml-4">Bluestery</h1></a>
+	{#if $user !== null}
+		<UserAvatar
+			url={$user?.avatar_url}
+			circle
+			class="absolute top-3 right-3 w-10 h-10 cursor-pointer"
+			on:click={() => mainMenuToggle.next()}
+		/>
+	{/if}
+</div>
+
+<SidebarMainMenu toggler={mainMenuToggle} />

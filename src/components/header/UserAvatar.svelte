9a2a9b947b03c36cb8010bf6e684542c13b2caa7
alessrdrgz <alessrodrgz@gmail.com>
2022-07-19 19:16:15 +0200

    :sparkles:feat: user avatar component

---
+++ src/components/header/UserAvatar.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let url: string | undefined;
+	export let circle: boolean = false;
+</script>
+
+<div
+	id="user-avatar"
+	class={`flex-shrink-0 flex-grow-0 basis-auto bg-contain shadow-md rounded ${
+		circle ? 'rounded-full' : 'rounded-lg'
+	} ${$$restProps.class}`}
+	style="background-image: url('{url}')"
+	on:click
+/>

3dc9e3aad85fe0338f7522495c8e2bb7ee2821f8
alessrdrgz <alessrodrgz@gmail.com>
2022-07-20 00:16:22 +0200

    :bug:fix: fixed url generation on production

--- src/routes/api/twilio/generate-invite-url.ts
+++ src/routes/api/twilio/generate-invite-url.ts
@@ -2,7 +2,7 @@ import type { RequestHandler } from '@sveltejs/kit';
 import { supabase } from '$services/supabase';
 import { Client } from '@twilio/conversations';
 import { generateAccessToken } from '$/routes/api/twilio/get-access-token';
-import * as JWT from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 
 const { VITE_PUBLIC_JWT_SECRET: JWT_SECRET, VITE_PUBLIC_DOMAIN: DOMAIN } = import.meta.env;
 
@@ -10,13 +10,13 @@ if (!JWT_SECRET) throw new Error('JWT env secret is not set');
 if (!DOMAIN) throw new Error('Domain env is not set');
 
 export const GET: RequestHandler = async ({ request }) => {
-	const jwt = request.headers.get('jwt');
-	if (!jwt) return { status: 401, body: { message: 'No se ha encontrado el usuario' } };
+	const jwToken = request.headers.get('jwt');
+	if (!jwToken) return { status: 401, body: { message: 'No se ha encontrado el usuario' } };
 
 	const sid = request.headers.get('sid');
 	if (!sid) return { status: 422, body: { message: 'No se ha encontrado la sala' } };
 
-	const user = await supabase.auth.api.getUser(jwt);
+	const user = await supabase.auth.api.getUser(jwToken);
 	if (!user) return { status: 401, body: { message: 'No se ha encontrado el usuario' } };
 
 	if (user.data?.id) {
@@ -27,7 +27,7 @@ export const GET: RequestHandler = async ({ request }) => {
 
 		if (conversation) {
 			if (conversationUser?.roleSid === 'RL18d041bac1f44010a8951b9347a0bc8f') {
-				const jsonWebToken = JWT.sign({ sid }, JWT_SECRET, { expiresIn: '15m' });
+				const jsonWebToken = jwt.sign({ sid }, JWT_SECRET, { expiresIn: '15m' });
 				return {
 					status: 200,
 					body: {

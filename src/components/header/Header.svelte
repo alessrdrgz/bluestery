2b7b9fa5004b4800490862a5ebe52d3639a9a85c
alessrdrgz <alessrodrgz@gmail.com>
2022-07-21 01:39:57 +0200

    :lipstick:style: changed close sidebar menu style

--- src/components/header/Header.svelte
+++ src/components/header/Header.svelte
@@ -3,20 +3,27 @@
 	import { user } from '$store/userStore';
 	import { useCycle } from 'svelte-motion';
 	import SidebarMainMenu from '$components/header/SidebarMenu/Main.svelte';
+	import CloseIcon from '@svicons/material-outlined/close.svelte';
 
-	const mainMenuToggle = useCycle(false, true);
+	const mainMenuToggler = useCycle(false, true);
 </script>
 
 <div class="h-[60px] bg-[#2c73d0] w-full border-b border-gray-300 flex items-center">
 	<a href="/"><h1 class="text-white text-2xl ml-4">Bluestery</h1></a>
 	{#if $user !== null}
-		<UserAvatar
-			url={$user?.avatar_url}
-			circle
-			class="absolute top-3 right-3 w-10 h-10 cursor-pointer"
-			on:click={() => mainMenuToggle.next()}
-		/>
+		{#if $mainMenuToggler}
+			<button on:click={() => mainMenuToggler.next()}>
+				<CloseIcon class="fill-white w-10 h-10 absolute top-3 right-3 cursor-pointer" />
+			</button>
+		{:else}
+			<UserAvatar
+				url={$user?.avatar_url}
+				circle
+				class="absolute top-3 right-3 w-10 h-10 cursor-pointer"
+				on:click={() => mainMenuToggler.next()}
+			/>
+		{/if}
 	{/if}
 </div>
 
-<SidebarMainMenu toggler={mainMenuToggle} />
+<SidebarMainMenu toggler={mainMenuToggler} />

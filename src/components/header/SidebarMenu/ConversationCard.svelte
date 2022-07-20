1f60777dc14ff37ef38188e22709f9816a9b48d9
alessrdrgz <alessrodrgz@gmail.com>
2022-07-21 01:46:54 +0200

    :sparkles:feat: conversation card info component

---
+++ src/components/header/SidebarMenu/ConversationCard.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let name: string;
+	export let lastMessage: string;
+	export let author: string;
+	export let unreadMessages: number = 0;
+</script>
+
+<a href={`/room/${name}`}>
+	<div class="border border-white rounded-lg mt-4 p-2 shadow-md bg-[#2867bb] relative">
+		<p class="text-white uppercase">{name}</p>
+		<p class="text-gray-300 overflow-x-clip">
+			{`${author}: ${lastMessage}`}
+		</p>
+		{#if unreadMessages > 0}
+			<span
+				class="text-white bg-red-500 rounded-full p-1 absolute -top-3 -right-2 w-6 h-6 text-center flex items-center justify-center"
+				>{unreadMessages}
+			</span>
+		{/if}
+	</div>
+</a>
